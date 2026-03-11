3c71f8f26665b73c75036a608826af828ae4bad4
Jonathan Janisch <jonjanisch@gmail.com>
2026-03-10 22:50:22 -0400

    Queue example

--- src/main/java/com/github/jonjanisch/GreetingResource.java
+++ src/main/java/com/github/jonjanisch/GreetingResource.java
@@ -1,6 +1,7 @@
 package com.github.jonjanisch;
 
 import dev.dbos.transact.DBOS;
+import dev.dbos.transact.StartWorkflowOptions;
 import dev.dbos.transact.workflow.WorkflowHandle;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -8,16 +9,26 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
 
     @Inject
     ExampleWorkflow exampleWorkflow;
 
+    @Path("/hello")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         var handle = DBOS.startWorkflow(() -> exampleWorkflow.runWorkflow());
         return "Started workflow with ID: " + handle.workflowId();
+    }
+
+    @Path("/queue")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String queue() {
+        var handle = DBOS.startWorkflow(() -> exampleWorkflow.runWorkflow(),
+                new StartWorkflowOptions().withQueue("exampleQueue"));
+        return "Started workflow on exampleQueue with ID: " + handle.workflowId();
     }
 }
